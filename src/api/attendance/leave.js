7574715c82be25ed326eb851ad7b075c4532a66e
renrenzi <Zinner2@163.com>
2023-03-06 22:00:45 +0800

    edit leave view

--- src/api/attendance/leave.js
+++ src/api/attendance/leave.js
@@ -11,3 +11,21 @@ export const pageLeaveList = (searchParam) => {
       return false
     })
 }
+export const updateLeaveInfo = (searchParam) => {
+  return service.post(PREFIX + '/update/leave/info', searchParam)
+    .then(res => {
+      return res.data
+    })
+    .catch(res => {
+      return false
+    })
+}
+export const batchDeleteLeave = (searchParam) => {
+  return service.post(PREFIX + '/batch/delete/leave/list', searchParam)
+    .then(res => {
+      return res.data
+    })
+    .catch(res => {
+      return false
+    })
+}
